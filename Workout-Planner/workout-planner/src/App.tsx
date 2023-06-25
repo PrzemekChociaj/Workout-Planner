de27e21fb470c9994b40827ff84462519f5f5733
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-06-25 19:52:29 +0200

    feat: logo

---
+++ Workout-Planner/workout-planner/src/App.tsx
@@ -0,0 +1,11 @@
+import Wrapper from './Wrapper';
+
+function App() {
+	return (
+		<>
+			<Wrapper />
+		</>
+	);
+}
+
+export default App;
---
+++ Workout-Planner/workout-planner/src/Wrapper.tsx
@@ -0,0 +1,12 @@
+
+import Navbar from './components/Navbar/Navbar';
+
+const Wrapper = () => {
+	return (
+		<div className=' h-screen bg-slate-300'>
+			<Navbar />
+		</div>
+	);
+};
+
+export default Wrapper;
