e2a67d058903644fe5b5b7c2678f735f002f33b0
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-06-28 18:04:06 +0200

    fix

--- Workout-Planner/workout-planner/src/Wrapper.tsx
+++ Workout-Planner/workout-planner/src/Wrapper.tsx
@@ -1,10 +1,13 @@
-
+import Calendar from './components/Calendar/Calendar';
+import Menu from './components/Menu/Menu';
 import Navbar from './components/Navbar/Navbar';
 
 const Wrapper = () => {
 	return (
 		<div className=' h-screen bg-slate-300'>
 			<Navbar />
+			<Menu />
+			<Calendar />
 		</div>
 	);
 };
